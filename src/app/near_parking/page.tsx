194797f2b7dcc414acb57573818004a3349a7481
leeminam32 <leeminam32@gmail.com>
2023-12-07 11:27:09 +0900

    Create page.tsx

---
+++ src/app/near_parking/page.tsx
@@ -0,0 +1,22 @@
+// import Link from "next/link";
+// import { UserContext } from "../context/LoginContext";
+// export default function Login() {
+//     const [accessToken, setAccessToken] = useState(null)
+//     const [loginType, setLoginType] = useState('')
+//   return (
+//     <div>
+//       <UserContext.Provider>
+//         <Link href="/auth/callback/git">깃</Link>
+//         <Link href="/auth/callback/google">구글</Link>
+//         <Link href="/auth/callback/kakao">카카오</Link>
+//       </UserContext.Provider>
+//     </div>
+//   );
+// }
+export default function NearParking() {
+    return (
+        <>
+            <h1>주변 주차장 페이지</h1>
+        </>
+    );
+}
