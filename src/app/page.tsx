08cc790069dd0f229866cb78ee3cefc2f980971a
leeminam32 <leeminam32@gmail.com>
2023-12-07 10:57:51 +0900

    feat: 배포 적용안됨

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,12 @@ export default function Home() {
             <div className="snap-y snap-mandatory h-screen w-screen overflow-auto">
                 <div className="snap-start bg-amber-200 w-screen h-screen flex items-center justify-center text-5xl">
                     {/*<Carousel/>*/}
+                    1
                 </div>
                 <div className="snap-start bg-amber-300 w-screen h-screen flex items-center justify-center text-5xl">2</div>
-                <div className="snap-start bg-amber-400 w-screen h-screen flex items-center justify-center text-5xl">1</div>
-                <div className="snap-start bg-amber-500 w-screen h-screen flex items-center justify-center text-5xl">1</div>
-                <div className="snap-start bg-amber-600 w-screen h-screen flex items-center justify-center text-5xl">1</div>
+                <div className="snap-start bg-amber-400 w-screen h-screen flex items-center justify-center text-5xl">3</div>
+                <div className="snap-start bg-amber-500 w-screen h-screen flex items-center justify-center text-5xl">4</div>
+                <div className="snap-start bg-amber-600 w-screen h-screen flex items-center justify-center text-5xl">5</div>
             </div>
             <div id="default-carousel" className="relative w-full h-screen" data-carousel="slide">
                 asdf
