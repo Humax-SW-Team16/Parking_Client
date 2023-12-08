ccc63324352bd83172348cad018fd39efb1ab11b
leeminam32 <leeminam32@gmail.com>
2023-12-08 14:10:25 +0900

    refactor: 주소 이름 통일 함수 추가

--- src/app/parking_detail/[id]/page.tsx
+++ src/app/parking_detail/[id]/page.tsx
@@ -1,6 +1,6 @@
 //2시간에 한번씩 초기화 돼서 주기적으로 갈아줘야 함
 import { cookies } from "next/headers";
-
+import addressModify from "@/app/utils/utils";
 export default async function Page({ params }: { params: { id: string } }) {
     const address =
         "http://3.34.236.224:3000/api/v1/parking/read/detail/" + params.id;
@@ -14,12 +14,12 @@ export default async function Page({ params }: { params: { id: string } }) {
             Cookie:CookieValue,
         },
     });
-
     const data = await res.json();
+
     //console.log("ID DATA#############################\n",data)
     return (
         <div className="bg-white">
-            <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+            <div className="mx-auto grid max-w-2xl grid-cols-1 items-stretch gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
                 <div className="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
                     <img
                         src="https://mediahub.seoul.go.kr/uploads/mediahub/2021/08/TmhmOrbRmPhwkuRTpyiMwRaYNEfWniPj.png"
@@ -46,9 +46,12 @@ export default async function Page({ params }: { params: { id: string } }) {
                     <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
                         {data.name}
                     </h2>
-                    <p className="mt-4 text-gray-500 line-clamp-3">{data.operation}</p>
+                    <div className="mt-4 text-gray-500 line-clamp-3">
+                        <p className="font-bold text-xl text-black">주소</p>
+                        {addressModify(data.address)}
+                    </div>
 
-                    <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
+                    <dl className="mt-9 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
                         {/*{features.map((feature) => (*/}
                         {/*    <div key={feature.name} className="border-t border-gray-200 pt-4">*/}
                         {/*        <dt className="font-medium text-gray-900">{feature.name}</dt>*/}
@@ -60,10 +63,6 @@ export default async function Page({ params }: { params: { id: string } }) {
                             <dt className="mt-2 text-sm text-gray-500">
                                 {data.operatingTime}
                             </dt>
-                        </div>
-                        <div className="border-t border-gray-200 pt-4 text-xl">
-                            <dt className="font-bold text-gray-900">주소</dt>
-                            <dt className="mt-2 text-sm text-gray-500">{data.address}</dt>
                         </div>
                         <div className="border-t border-gray-200 pt-4 text-xl">
                             <dt className="font-bold text-gray-900">기본 이용요금</dt>
---
+++ src/app/utils/utils.tsx
@@ -0,0 +1,32 @@
+function addressModify(address: string): string {
+    var words = address.split(' ')
+    words[0] = addressFullName(words[0])
+    return words.join(" ")
+}
+function addressFullName(add: string): string {
+    const names = {
+        "대전광역시": ["대전", "대전시", "대전직할시"],
+        "서울특별시": ["서울", "서울시", "Seoul"],
+        "인천광역시": ["인천", "인천시", "Incheon", "ICN"],
+        "광주광역시": ["광주", "광주시", "Gwangju"],
+        "대구광역시": ["대구", "대구시", "대구직할시"],
+        "울산광역시": ["울산", "울산시", "울산직할시"],
+        "부산광역시": ["부산", "부산시", "Busan"],
+        "경기도": ["경기"],
+        "강원도": ["강원"],
+        "충청북도": ["충북"],
+        "충청남도": ["충남"],
+        "전라북도": ["전북"],
+        "전라남도": ["전남"],
+        "경상북도": ["경북"],
+        "경상남도": ["경남"]
+    }
+    for (var city in names) {
+        if (names[city].includes(add)) {
+            return city
+        }
+    }
+    return add
+}
+
+export default addressModify
