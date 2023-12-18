f59aa502c2789492af73505820d9326ca856dfe1
FordangIT <fordang0819@gmail.com>
2023-12-18 16:22:07 +0900

    feat: 레이아웃 파일

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,30 @@
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { UserLocationProvider } from "./context/userLocationContext";
+import "./globals.css";
 import NavigationBar from "@/app/components/NavigationBar";
 import Footer from "@/app/components/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+export const metadata: Metadata = {
+  title: "Humax Parking App",
+  description: "humax parking app by Team 16",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>
-        <NavigationBar/>
-      {children}
-      <Footer/>
-      </body>
-    </html>
-  )
+    <UserLocationProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <NavigationBar />
+          {children}
+          <Footer />
+        </body>
+      </html>
+    </UserLocationProvider>
+  );
 }
